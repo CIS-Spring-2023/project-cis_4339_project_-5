bb0c93a025109e3a509dc04b2def8a0e6dc19190
bjrshussain <bjrshussain@outlook.com>
2023-03-13 19:05:58 -0500

    Fixed login bug

--- frontend/src/components/loginPage.vue
+++ frontend/src/components/loginPage.vue
@@ -1,68 +1,70 @@
 <!-- check the if someonme is logged in if yes not rendeger the lolgin page -->
-<template v-if="!store.EisLoggedIn && !store.VisLoggedIn">
-  <div class="my-10">
-    <h1
-      class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
-    >
-      Staff Login
-    </h1>
-  </div>
-
-  <div class="w-full max-w-lg mx-auto px-4">
-    <form @submit.prevent="handleLogin" novalidate="true">
-      <div class="form-group">
-        <label for="username"> Username </label>
-        <input
-          type="text"
-          id="username"
-          v-model="username"
-          class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 hover:cursor-pointer"
-          placeholder="Enter Username"
-          required
-        />
-        <span class="text-black" v-if="v$.username.$error">
-          <p
-            class="text-red-700"
-            v-for="error of v$.username.$errors"
-            :key="error.$uid"
-          >
-            {{ error.$message }}!
-          </p> </span
-        ><br /><br />
-      </div>
-      <div class="form-group">
-        <label for="password"> Password </label>
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 hover:cursor-pointer"
-          placeholder="Enter Password"
-          required
-        />
-        <span class="text-black" v-if="v$.password.$error">
-          <p
-            class="text-red-700"
-            v-for="error of v$.password.$errors"
-            :key="error.$uid"
-          >
-            {{ error.$message }}!
-          </p> </span
-        ><br /><br />
-      </div>
-      <button
-        class="bg-red-700 text-white rounded hover:bg-red-600"
-        type="submit"
+<template>
+  <main v-if="!store.EisLoggedIn && !store.VisLoggedIn">
+    <div class="my-10">
+      <h1
+        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
       >
-        Login
-      </button>
-    </form>
+        Staff Login
+      </h1>
+    </div>
+
+    <div class="w-full max-w-lg mx-auto px-4">
+      <form @submit.prevent="handleLogin" novalidate="true">
+        <div class="form-group">
+          <label for="username"> Username </label>
+          <input
+            type="text"
+            id="username"
+            v-model="username"
+            class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 hover:cursor-pointer"
+            placeholder="Enter Username"
+            required
+          />
+          <span class="text-black" v-if="v$.username.$error">
+            <p
+              class="text-red-700"
+              v-for="error of v$.username.$errors"
+              :key="error.$uid"
+            >
+              {{ error.$message }}!
+            </p> </span
+          ><br /><br />
+        </div>
+        <div class="form-group">
+          <label for="password"> Password </label>
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 hover:cursor-pointer"
+            placeholder="Enter Password"
+            required
+          />
+          <span class="text-black" v-if="v$.password.$error">
+            <p
+              class="text-red-700"
+              v-for="error of v$.password.$errors"
+              :key="error.$uid"
+            >
+              {{ error.$message }}!
+            </p> </span
+          ><br /><br />
+        </div>
+        <button
+          class="bg-red-700 text-white rounded hover:bg-red-600"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  </main>
+  <!-- redirect to home page if someone is already logged in -->
+  <div v-else>
+    {{ $router.push("/") }}
   </div>
 </template>
-<!-- redirect to home page if someone is already logged in -->
-<div v-else>
-    {{ $router.push("/") }}
-</div>
 
 <script>
 import { useLoggedInUserStore } from "@/store/loggedInUser";
